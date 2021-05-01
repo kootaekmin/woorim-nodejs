3e5949b485886b894c0bd00c88214a6585559db6
kootaekmin <kootaekmin@gmail.com>
2021-05-01 20:25:54 +0900

    L-18

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 // module
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // routing
@@ -8,6 +9,9 @@ const home = require('./src/routes/home');
 // app setting
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', home); // wiring middleware
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 'use strict';
 
-const hello = (req, res) => {
-	res.render('home/index');
+const users = {
+	id: ['woorim', 'junior', 'senior'],
+	psword: ['1234', '1234', '123456'],
 };
 
-const login = (req, res) => {
-	res.render('home/login');
+const output = {
+	hello: (req, res) => {
+		res.render('home/index');
+	},
+
+	login: (req, res) => {
+		res.render('home/login');
+	},
+};
+
+const process = {
+	login: (req, res) => {
+		const id = req.body.id,
+			psword = req.body.psword;
+
+		if (users.id.includes(id)) {
+			const idx = users.id.indexOf(id);
+			if (users.psword[idx] === psword) {
+				return res.json({
+					success: true,
+				});
+			}
+		}
+
+		return res.join({
+			success: false,
+			msg: 'failed to login',
+		});
+	},
 };
 
 module.exports = {
-	hello,
-	login,
+	output,
+	process,
 };
